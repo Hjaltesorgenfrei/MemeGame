4cd49cdfde7c302a1fa93aa1920af27a4b701bd1
Hjalte Sorgenfrei Mac Dalland <hjaltesorgenfrei@gmail.com>
2022-06-14 18:43:41 +0200

    Test something else

--- server.py
+++ server.py
@@ -1,37 +1,32 @@
 #!/usr/bin/env python
 
-import asyncio, websockets, os
+import asyncio
+import signal
+import os
+import signal
+import websockets
 
 
-SERVER_ADDRESS = '127.0.0.1'
-HTTP_PORT = int(os.environ.get("PORT", 80))
-WEBSOCKET_PORT = 5678
+async def echo(websocket):
+    async for message in websocket:
+        await websocket.send(message)
 
-
-async def handle_http(reader, writer):
-  data = await reader.read(100)
-  message = data.decode()
-  writer.write(data)
-  await writer.drain()
-  writer.close()
-
-
-async def ws_callback(websocket, path):
-  global parsed_data
-  while True:
-      data = await websocket.recv()
-      # How do I access parsed_data in the main function below
-      # parsed_data = json.loads(data)
-      await websocket.send(data)
 
 async def main():
-  ws_server = await websockets.serve(ws_callback, SERVER_ADDRESS, WEBSOCKET_PORT)
-  print(f'Websocket server listening on port {WEBSOCKET_PORT}')
-  http_server = await asyncio.start_server(
-      handle_http, SERVER_ADDRESS, HTTP_PORT)
-  print(f'HTTP server listening on port {HTTP_PORT}')
-  await asyncio.Future()  # run forever
+    # Set the stop condition when receiving SIGTERM.
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    signal.signal(signal.SIGTERM, lambda *args: loop.stop())
+    signal.signal(signal.SIGINT, lambda *args: loop.stop())
+    listen_port = int(os.environ.get("PORT", 5678))
+    async with websockets.serve(
+        echo,
+        host="",
+        port= listen_port,
+    ):
+      print(f"Listing on {listen_port}")
+      await stop
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
