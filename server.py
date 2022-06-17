074d9a269625faec7a408fefadbfab6e0706760d
Hjalte Sorgenfrei Mac Dalland <hjaltesorgenfrei@gmail.com>
2022-06-17 16:51:26 +0200

    Track usernames

--- server.py
+++ server.py
@@ -4,40 +4,55 @@ import asyncio
 import signal
 import json
 import os
+import websockets.server
 import websockets
 import httpx
 import requests
 
+
 async def get_json_from_url_async(url):
-  async with httpx.AsyncClient() as client:
-    response = await client.get(url)
-    return json.loads(response.content.decode("utf-8"))
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        return json.loads(response.content.decode("utf-8"))
+
 
 def get_json_from_url(url):
-  response = requests.get(url)
-  return json.loads(response.content.decode("utf-8"))
+    response = requests.get(url)
+    return json.loads(response.content.decode("utf-8"))
+
 
 def json_array_from_dict(dic):
-  return json.dumps([{'key' : k, 'value' : dic[k]} for k in dic])
-
-visuals = {x["id"]:x["filename"] for x in get_json_from_url("https://api.mads.monster/visuals")}
-top_texts = {x["id"]:x["memetext"] for x in get_json_from_url("https://api.mads.monster/toptexts")}
-bottoms_texts = {x["id"]:x["memetext"] for x in get_json_from_url("https://api.mads.monster/bottomtexts")}
-
-print(json_array_from_dict(bottoms_texts))
+    return json.dumps([{'key': k, 'value': dic[k]} for k in dic])
 
 
+visuals = {x["id"]: x["filename"]
+           for x in get_json_from_url("https://api.mads.monster/visuals")}
+top_texts = {x["id"]: x["memetext"]
+             for x in get_json_from_url("https://api.mads.monster/toptexts")}
+bottoms_texts = {x["id"]: x["memetext"]
+                 for x in get_json_from_url("https://api.mads.monster/bottomtexts")}
 
-async def echo(websocket):
+connections = []
+
+async def receive_messages(websocket):
+    connections.append(websocket)
     async for message in websocket:
-        if message.startswith("visuals"):
-          await websocket.send(json_array_from_dict(visuals))
-        elif message.startswith("toptexts"):
-          await websocket.send(json_array_from_dict(top_texts))
-        elif message.startswith("bottomtexts"):
-          await websocket.send(json_array_from_dict(bottoms_texts))
-        else:
-          await websocket.send(message)
+        try:
+            data = json.loads(message)
+        except:
+            print("Failed on:", message)
+            continue
+        match (data["type"]):
+            case "username_change_request": 
+                response = json.dumps({
+                    "type":"username_change_approve",
+                    "username": data["username"],
+                    "user_id": data["user_id"]
+                    })
+                websockets.broadcast(connections, response)
+                print(data["user_id"], "changed name to", data["username"])
+            case _:
+                print(data)
 
 
 async def main():
@@ -46,13 +61,13 @@ async def main():
     signal.signal(signal.SIGTERM, lambda *args: loop.stop())
     signal.signal(signal.SIGINT, lambda *args: loop.stop())
     listen_port = int(os.environ.get("PORT", 5678))
-    async with websockets.serve(
-        echo,
+    async with websockets.server.serve(
+        receive_messages,
         host="",
         port=listen_port,
     ):
-      print(f"Listing on {listen_port}")
-      await stop
+        print(f"Listing on {listen_port}")
+        await stop
 
 
 if __name__ == "__main__":
--- website/script.js
+++ website/script.js
@@ -1,12 +1,87 @@
+// let socket = new WebSocket("ws://mega-meme-game.herokuapp.com/");
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; SameSite=Strict; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {   
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+
+function generate_guid() {
+    let id = makeid(16);
+    setCookie("user_guid", id, 30);
+    return id;
+}
+
+let own_user_id = getCookie("user_guid") ?? generate_guid();
+
+function username_changed(username, user_id) {
+    if (own_user_id == user_id) {
+        setCookie("username", username, 30);
+        show_username.innerHTML = username;
+    }
+    console.log(`User(${user_id}) changed name to ${username}`)
+}
+
+
+// Socket Setup
 let socket = new WebSocket("ws://localhost:5678");
 socket.addEventListener('open', function (event) {
-    socket.send('toptexts');
+    // socket.send('toptexts');
 });
 socket.addEventListener('message', function (event) {
     let data = JSON.parse(event.data);
-    for (const meme in data) {
-        document.body.innerHTML += data[meme].value + "<br>"
-    }
-    console.log('Message from server ');
+    switch(data.type) {
+        case 'username_change_approve':
+            username_changed(data.username, data.user_id)
+        default: 
+            console.log("Unhandled data", data);
+    }   
 });
 
+
+
+
+let show_username = document.getElementById('show_username');
+saved_username = getCookie("username");
+if (saved_username !== null) {
+    show_username.innerHTML = saved_username;
+}
+
+function saveNameChange() {
+    let input = document.querySelector('input');
+    username = input.value;
+    if (socket.readyState == socket.OPEN) {
+        let message = {
+            type: 'username_change_request',
+            username: username,
+            user_id: own_user_id
+        };
+        socket.send(JSON.stringify(message))
+    }
+}
