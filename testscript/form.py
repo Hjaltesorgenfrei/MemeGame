a73442f30a47f1efb8804166f662f4662f4d91f6
Hjalte Sorgenfrei Mac Dalland <hjaltesorgenfrei@gmail.com>
2023-03-23 14:33:03 +0100

    Submit multi images

--- testscript/form.py
+++ testscript/form.py
@@ -1,15 +1,24 @@
 from socket import timeout
 import requests
+import os
 
-with open("IMG.JPG","rb") as file:
-    data = {'visualFile': ("IMG.JPG", file.read(), "image/jpeg") }
-    body = {'toptext': "", 'bottomtext':""}
-    request = requests.Request('POST',"https://api.mads.monster/Upload/Memes", files= data, data=body ).prepare()
-    print(request.body[:300])
-    s = requests.Session()
-    response = s.send(request)
-    print(response)
-    print(response.content)
+for f in os.listdir("./images"):
+    with open(f"./images/{f}","rb") as file:
+        filend = f.split(".")[1]
+        if filend not in ["jpg","jpeg","png"]:
+            continue
+        if filend == "jpg": 
+            filend = "jpeg"
+        data = {'visualFile': (f, file.read(), f"image/{filend}") }
+        body = {'toptext': "", 'bottomtext':""}
+        request = requests.Request('POST',"https://api.mads.monster/Upload/Memes", files= data, data=body ).prepare()
+        print(request.body[:300])
+        s = requests.Session()
+        response = s.send(request)
+        response.raise_for_status()
+        print(response)
+        print(response.content)
+    os.remove(f"./images/{f}")
     
 
 """
